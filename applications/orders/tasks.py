d82a50b11d0e5c82baad4c2ed32b9c350c48e305
Haron098 <alkeevvalba@gmail.com>
2023-01-23 18:56:30 +0600

    tasks

--- applications/orders/tasks.py
+++ applications/orders/tasks.py
@@ -5,7 +5,7 @@ from config.celery import app
 
 @app.task
 def send_confirm_link(email, confirm_code):
-    full_link = f'{config("ALLOWED_HOSTS").split(",")[1]}:{config("MAIN_PORT")}/api/v1/order/confirm/{confirm_code}/'
+    full_link = f'http/{config("ALLOWED_HOSTS").split(",")[1]}:{config("MAIN_PORT")}/api/v1/order/confirm/{confirm_code}/'
     send_mail(
         'Ссылка для подтверждение заказа',
         full_link,
